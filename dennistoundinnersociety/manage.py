3f13fc5b42be4d8a14bfe296bc4b0274a1c916f0
DanielPolatajko <danielpolatajko@gmail.com>
2021-04-10 17:10:29 +0100

    trying iframe

--- dennistoundinnersociety/manage.py
+++ dennistoundinnersociety/manage.py
@@ -8,7 +8,7 @@ from dotenv import load_dotenv
 
 def main():
     """Run administrative tasks."""
-    load_dotenv('.env.prod')
+    load_dotenv('.env.dev')
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'dennistoundinnersociety.settings')
     try:
         from django.core.management import execute_from_command_line
